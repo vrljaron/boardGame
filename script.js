1235a09c1fd125615a42ccfc88f33be77f0ea45c
Váraljai Áron <h986133@stud.u-szeged.hu>
2022-03-28 17:13:04 +0200

    events, click on field and player class

--- script.js
+++ script.js
@@ -1,30 +1,77 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-let mouse
+const bField = document.getElementById('field')
+const bPlayer = document.getElementById('player')
+const modalEl = document.getElementById('modalEl')
+const houseEl = document.getElementById('houseEl')
+const farmEl = document.getElementById('farmEl')
+const foresterEl = document.getElementById('foresterEl')
+const mineEL = document.getElementById('mineEl')
 
 canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.height = 800
 
-const fields = []
+let fields = []
+let player
+let clicked
+let mouse
 
-class field {
-    constructor(x, y, name) {
-        this.x = x
-        this.y = y
-        this.name = name
+class Player {
+    constructor(fields, food, rock, wood, population, happiness) {
+        this.fields = fields
+        this.food = food
+        this.rock = rock
+        this.wood = wood
+        this.population = population
+        this.happiness = happiness
+    }
+
+    toString = function () {
+        return "Population: " + this.population + " Happiness: " + this.happiness + " Food: " + this.food + " Wood: " + this.wood + " Rock: " + this.rock
+    }
+
+    innerUpdate = function () {
+        bPlayer.innerText = player.toString()
+    }
+
+    growth = function () {
+
     }
 }
 
+class field {
+    constructor(x, y, name, color) {
+        this.x = x
+        this.y = y
+        this.name = name
+        this.color = color
+    }
+}
+
+function randPos() {
+    let x = Math.floor(Math.random() * 10)
+    let y = Math.floor(Math.random() * 10)
+    return {x, y}
+}
+
 function init() {
-    let x = 500, y = 200
+    let x = canvas.width / 3, y = 200
+    let rand = randPos()
+    player = new Player([], 5, 3, 3, 3, 5)
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
-            fields.push(new field(x, y, 'empty' + x + ':' + y))
+            if (i === rand.x && j === rand.y) {
+                player.fields.push([x, y, 'green'])
+                fields.push(new field(x, y, 'Town', 'green'))
+            } else {
+                fields.push(new field(x, y, 'empty', 'gray'))
+            }
             y += 50
         }
         y = 200
         x += 50
     }
+    bPlayer.innerText = player.toString()
 }
 
 
@@ -32,7 +79,7 @@ function draw() {
     fields.forEach((field) => {
         c.beginPath()
         c.rect(field.x, field.y, 50, 50)
-        c.fillStyle = 'gray'
+        c.fillStyle = field.color
         c.fill()
         c.stroke()
     })
@@ -40,21 +87,63 @@ function draw() {
 
 function getField(event) {
     fields.forEach((field) => {
-        if (field.x <= event.clientX && field.x + 50 >= event.clientX) {
-            if (field.y <= event.clientY && field.y + 50 >= event.clientY) {
-                console.log(field.name)
+        if (event.type === 'click') {
+            if (field.x <= event.clientX && field.x + 50 >= event.clientX) {
+                if (field.y + 48 <= event.clientY && field.y + 98 >= event.clientY) {
+                    if (field.name === 'empty') {
+                        mouse = true
+                    }
+                }
+            }
+        } else {
+            if (field.x <= event.clientX && field.x + 50 >= event.clientX) {
+                if (field.y + 48 <= event.clientY && field.y + 98 >= event.clientY) {
+                    bField.innerText = field.name
+                }
             }
         }
     })
 }
 
+let animationId
+
 function render() {
-    requestAnimationFrame(render)
+    animationId = requestAnimationFrame(render)
     draw()
+    if (clicked) {
+        if (mouse) {
+            cancelAnimationFrame(animationId)
+            modalEl.style.display = 'flex'
+        }
+    }
 }
 
 addEventListener('mousemove', (event) => {
     getField(event)
+})
+
+addEventListener('click', (event) => {
+    if (clicked) {
+        clicked = false
+        modalEl.style.display = 'none'
+        render()
+    } else {
+        getField(event)
+        console.log(mouse)
+        clicked = true
+    }
+})
+
+farmEl.addEventListener('click', () => {
+})
+
+houseEl.addEventListener('click', () => {
+})
+
+foresterEl.addEventListener('click', () => {
+})
+
+mineEL.addEventListener('click', () => {
 })
 
 
